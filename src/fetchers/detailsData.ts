0a5e712b3a170c2d396a8cf03ab41d59dece14c9
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-04-13 15:06:11 +0200

    feat: detail fetching function

---
+++ src/fetchers/detailsData.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { HomeData } from "./homeDatas";
+
+export interface DetailsData extends HomeData {
+    languages: string[];
+    currencies: {
+        [key: string]: {
+            name: string;
+            symbol: string
+        }
+    };
+    subregion: string;
+    tld: string[];
+
+}
+
+export const CountryDetailsData = async (name: string) => {
+    const response = await axios.get(`https://restcountries.com/v3.1/name/${name}?fields=name,population,region,languages,currencies,capital,subregion,tld`);
+
+    const countryDetails: DetailsData = response.data 
+    return countryDetails;
+}
